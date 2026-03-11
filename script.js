d8b203a1efe81b9d788cc56a1e81fcfd6c1d8914
ayman-ap <ayman.ap.dev@gmail.com>
2026-03-11 16:55:38 +0300

    Remove debug-aiding code
    - Remnants of a finished building

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-// To debug using console.log, find the relevant debug-aiding code above "Debug conclusion: ..." comments.
-
 let humanScore = 0;
 let computerScore = 0;
 let drawCount = 0;
@@ -9,47 +7,30 @@ function getHumanChoice() {
     // humanChoice.trim().toLowerCase() incase I change my mind.
 
     let humanChoice = parseInt(prompt("Enter:\n1. For Rock\n2. For paper\n3. For scissors", 1));
-    // console.log(humanChoice);
-    // Debug conclusion: Works as intended.
     return humanChoice;
 }
 
 function getComputerChoice() {
     let computerChoice = Math.floor(Math.random() * 3) + 1;
-    // console.log(computerChoice);
-    // Debug conclusion: Works as intended.
     return computerChoice;
 }
 
 function playRound(humanChoice, computerChoice) {
     console.log(computerChoice);
     console.log(humanChoice);
-    // Debug conclusion: Value recieved as intended.
 
     if(isNaN(humanChoice)) {
         console.log("Please enter a valid number.");
-        // Works as intended.
         return;
     } else if (humanChoice <= 0 || humanChoice > 3) {
         console.log("Please enter a number between 1 - 3!");
-        // Works as intended.
         return;
     } else {
         function checkIfHumanWon() {
-            // return "draw";
-            // return true;
-            //return false;
-            // uncomment (relevant) above comment to debug incrementation.
-
             //filter out draw possibilities.
             if(computerChoice === humanChoice) {
                 return "draw";
             } 
-
-            // console.log(`y = ${computerChoice}`);
-            // console.log(`x = ${humanChoice}`);
-            // Above logs to aid in debugging the logic explained below.
-            // Debug conclusion: Logic works as intended.
 
             // let human be x, computer be y.
             // let 1. be rock; 2. be paper; 3. be scissors.
@@ -70,16 +51,11 @@ function playRound(humanChoice, computerChoice) {
 
             return false;
         }
-        // console.log(checkIfHumanWon());
-        // Debug conclusion: Function Works as intended.
 
         function getConvertedChoiceFromNumberToStringCounterpart(choiceInNumberForm) {
             let choices = ["Rock", "Paper", "Scissors"];
             return choices[choiceInNumberForm - 1];
         }
-        // console.log(getConvertedChoiceFromNumberToStringCounterpart(humanChoice));
-        // console.log(getConvertedChoiceFromNumberToStringCounterpart(computerChoice));
-        // Debug conclusion: Function Works as intended.
 
         function showVictoryMessage() {
             let humanStringChoice = getConvertedChoiceFromNumberToStringCounterpart(humanChoice);
@@ -87,8 +63,6 @@ function playRound(humanChoice, computerChoice) {
             let victoryMessage = `You Won This Round!\nYou chose ${humanStringChoice}, computer chose ${computerStringChoice}.\n${humanStringChoice} beats ${computerStringChoice}!`;
             console.log(victoryMessage);
         }
-        // showVictoryMessage();
-        // Debug conclusion: Function Works as intended.
 
         function showDefeatMessage() {
             let humanStringChoice = getConvertedChoiceFromNumberToStringCounterpart(humanChoice);
@@ -96,52 +70,32 @@ function playRound(humanChoice, computerChoice) {
             let defeatMessage = `You Lost This Round!\nYou chose ${humanStringChoice}, computer chose ${computerStringChoice}.\n${computerStringChoice} beats ${humanStringChoice}!`;
             console.log(defeatMessage);
         }
-        // showDefeatMessage();
-        // Debug conclusion: Function Works as intended.
 
         function showScores() {
             let showScoresMessage = `Your Score: ${humanScore}\nComputerScore: ${computerScore}\nDraw count: ${drawCount}`;
             console.log(showScoresMessage);
         }
-        // showScores();
-        // Debug conclusion: Function works as intended.
-
 
         if(checkIfHumanWon() === "draw") {
             drawCount++;
-            // Uncomment; return "draw", in checkIfHumanWon() to test this.
-            // console.log(drawCount);
-            // Debug conclusion: Incrementation works as intended.
-
             console.log(`It's a draw!`);
         } else if(checkIfHumanWon()) {
             humanScore++;
-            // Uncomment; return true; in checkIfHumanWon() to test this.
-            // console.log(humanScore);
-            // Debug conclusion: Incrementation works as intended.
-
             showVictoryMessage();
         } else {
             computerScore++;
-            // Uncomment; return false; in checkIfHumanWon() to test this.
-            // console.log(computerScore);
-            // Debug conclusion: Incrementation works as intended.
             showDefeatMessage();
         }
 
         showScores();
         return;
     }
-
-    // console.log("Logic Error: The code execution is not supposed to reach here.");
-    // return;
 }
 
 function playGame() {
     let numberOfRounds = 5;
 
     for(let i = 0; i < numberOfRounds; i++){
-        // Debug conclusion: Works as intended.
         playRound(getHumanChoice(), getComputerChoice());
     }
 
